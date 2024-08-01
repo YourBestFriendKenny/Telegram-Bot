38c2a7c383bd8e779915370299358beec86a9f81
makima <136541339+YourBestFriendKenny@users.noreply.github.com>
2024-08-01 23:24:43 +0300

    create filter
    as function, put her in decorator @dp.message(some_filter)

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from aiogram.filters import Command
 from aiogram.types import Message, ContentType
 from aiogram import F
 
+ #print(message.model_dump_json(indent=4, exclude_none=True)) # - эта строчка позволяет получить апдейт от пользователя
 
 BOT_TOKEN = '7229544704:AAH4F8HNIJVMPMxJqlnGEErPOQXRFZS-syQ'
 
@@ -11,33 +12,24 @@ bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
 
-async def process_start_command(message: Message):
-    await message.answer("Hello, my name is echo-bot, write me someting")
+
+
+def start_filter(message: Message) -> bool:
+    return message.text == '/start'
+
+    
 
 
 
-async def process_help_command(message: Message):
-    await message.answer("Write me something and i'll reply you your message")
-
-
-
-
-
-@dp.message()
-async def send_echo(message: Message):
-    try: 
-        await message.send_copy(chat_id=message.chat.id)
-    except TypeError:
-        await message.reply(text=' This type of update is not supported ')
-
+@dp.message(start_filter)
+async def start_command(message: Message):
+    await message.answer(text='this is commmand start')
 
 
 
 
 if __name__ == '__main__':
     dp.run_polling(bot)
-
-
 
 
 
