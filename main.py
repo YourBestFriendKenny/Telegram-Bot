81a6580520a618298bd2d648dfbdd1abb621200f
makima <136541339+YourBestFriendKenny@users.noreply.github.com>
2024-08-02 00:21:19 +0300

    few changes
    so, i changed bot token after commited.
    XD XD XD

--- main.py
+++ main.py
@@ -1,29 +1,17 @@
 from aiogram import Bot, Dispatcher
-from aiogram.filters import Command
-from aiogram.types import Message, ContentType
-from aiogram import F
+from aiogram.filters import BaseFilter, CommandStart
+from aiogram.types import Message
+from w_sourse.bot_token import BOT_TOKEN
+from w_sourse.bot_token import TELEGRAM_ID
 
- #print(message.model_dump_json(indent=4, exclude_none=True)) # - эта строчка позволяет получить апдейт от пользователя
-
-BOT_TOKEN = '7229544704:AAH4F8HNIJVMPMxJqlnGEErPOQXRFZS-syQ'
 
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
+admin_ids: list[int] = [TELEGRAM_ID]
 
 
-
-def start_filter(message: Message) -> bool:
-    return message.text == '/start'
-
-    
-
-
-
-@dp.message(start_filter)
-async def start_command(message: Message):
-    await message.answer(text='this is commmand start')
 
 
 
